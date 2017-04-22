a30c92a9c119832e7a323d6e05ca867a257b53f9
Darenn Keller <keller.darenn@gmail.com>
2017-04-22 13:19:21 +0200

    Base gameplay. Planet + player moving.

---
+++ Assets/Scripts/ApplyGravityForce.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class ApplyGravityForce : MonoBehaviour {
+
+    public float GravityForce;
+
+    private List<Rigidbody2D> inRangeObjects;
+
+    private void Awake()
+    {
+        inRangeObjects = new List<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        foreach(Rigidbody2D rb in inRangeObjects)
+        {
+            rb.AddForce((transform.position - rb.transform.position).normalized * GravityForce);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D otherRigibody = other.GetComponent<Rigidbody2D>();
+        if (otherRigibody)
+        {
+            inRangeObjects.Add(otherRigibody);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        inRangeObjects.Remove(other.GetComponent<Rigidbody2D>());
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        // Modify the rotation
+        Vector2 C = transform.position.normalized - other.transform.position;
+        float Angle = Mathf.Atan2(C.y, C.x);
+        float AngleInDegrees = Angle * Mathf.Rad2Deg;
+        Debug.Log("Atan2 Angle in degrees " + AngleInDegrees);
+        other.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90 + AngleInDegrees));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+    public float MovingForce;
+    public float MaxVelocity;
+    public float JumpForce;
+
+    private Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rigidbody.AddRelativeForce(new Vector2(MovingForce * move.x, 0));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddRelativeForce(new Vector2(0, JumpForce));
+        }
+
+        if (Mathf.Abs(rigidbody.velocity.x) > MaxVelocity)
+        {
+            rigidbody.velocity = new Vector2(MaxVelocity * Mathf.Sign(rigidbody.velocity.x), rigidbody.velocity.y);
+        }
+
+        if (Mathf.Abs(rigidbody.velocity.y) > MaxVelocity)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, MaxVelocity * Mathf.Sign(rigidbody.velocity.y));
+        }
+    }
+}
